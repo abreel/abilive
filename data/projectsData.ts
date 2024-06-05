6bd696b96e1c94c4a979498cc489c3db843f7752
abreel <64946264+abreel@users.noreply.github.com>
2024-06-05 08:33:29 +0100

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,7 +10,7 @@ const projectsData: Project[] = [
     title: 'AI-Powered Text Editor with Advanced Image and Conversation Integration',
     description: `A Figma prototype of an innovative text editor that integrates AI for text generation, image search and creation, and conversational interaction, streamlining content creation and enhancing user engagement.`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.figma.com/proto/4lPHehd0k3ETHN2mwLhqHo?node-id=2004:800',
+    href: '/blog/the-time-machine',  
   },
   {
     title: 'The Time Machine',
