228f4e8cd08344c3fcbe06b605b4d645da6d061d
abreel <64946264+abreel@users.noreply.github.com>
2024-06-06 14:07:13 +0100

    updated spinner

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,5 +1,30 @@
-export default function LoadingSpinner() {
-  return <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
-          <div className="text-lg text-white">Loading...</div>
-        </div>
+import React from 'react'
+
+const LoadingSpinner: React.FC = () => {
+  return (
+    <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-white bg-opacity-70 dark:bg-gray-950">
+      <svg
+        className="h-8 w-8 animate-spin text-orange-500 dark:text-gray-300 sm:h-16 sm:w-16"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20 12a8 8 0 01-8 8v4c6.627 0 12-5.373 12-12h-4zm-6-7.938A7.962 7.962 0 0120 12h4c0-3.042-1.135-5.824-3-7.938l-3 2.647z"
+        ></path>
+      </svg>
+    </div>
+  )
 }
+
+export default LoadingSpinner
