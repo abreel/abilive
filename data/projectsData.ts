5fdc7e40db925ce18d0318e4879823b519eb53ad
abreel <64946264+abreel@users.noreply.github.com>
2024-06-06 15:14:29 +0100

    duplicated projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,6 +12,18 @@ const projectsData: Project[] = [
     imgSrc: 'https://cdn.loom.com/sessions/thumbnails/6e7d7af9e72c45c39a13fe4110bfe801-with-play.gif',
     href: '/blog/ai-text-editor',
   },
+  {
+    title: 'AI-Powered Text Editor with Advanced Image and Conversation Integration',
+    description: `A Figma prototype of an innovative text editor that integrates AI for text generation, image search and creation, and conversational interaction, streamlining content creation and enhancing user engagement.`,
+    imgSrc: 'https://cdn.loom.com/sessions/thumbnails/6e7d7af9e72c45c39a13fe4110bfe801-with-play.gif',
+    href: '/blog/ai-text-editor',
+  },
+  {
+    title: 'AI-Powered Text Editor with Advanced Image and Conversation Integration',
+    description: `A Figma prototype of an innovative text editor that integrates AI for text generation, image search and creation, and conversational interaction, streamlining content creation and enhancing user engagement.`,
+    imgSrc: 'https://cdn.loom.com/sessions/thumbnails/6e7d7af9e72c45c39a13fe4110bfe801-with-play.gif',
+    href: '/blog/ai-text-editor',
+  },
 ]
 
 export default projectsData
