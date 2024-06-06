72800f2b7bf435889d2d50fbd150f2e9a6428131
abreel <64946264+abreel@users.noreply.github.com>
2024-06-06 13:34:21 +0100

    fixed import statement bug & used suspense for loom video

--- app/projects/loading.tsx
+++ app/projects/loading.tsx
@@ -1,4 +1,4 @@
-import LoadingSpinner from "@/components/LoadingSpinner.tsx";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
