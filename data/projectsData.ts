3daa0c913c49bf05649a339e029c577544b956c5
abreel <64946264+abreel@users.noreply.github.com>
2024-06-06 11:42:16 +0100

    Tried using a gif for project thumbnail

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,8 +9,8 @@ const projectsData: Project[] = [
   {
     title: 'AI-Powered Text Editor with Advanced Image and Conversation Integration',
     description: `A Figma prototype of an innovative text editor that integrates AI for text generation, image search and creation, and conversational interaction, streamlining content creation and enhancing user engagement.`,
-    imgSrc: '/static/images/time-machine',
-    href: '/blog/the-time-machine',  
+    imgSrc: 'https://cdn.loom.com/sessions/thumbnails/6e7d7af9e72c45c39a13fe4110bfe801-with-play.gif',
+    href: '/blog/ai-text-editor',
   },
 ]
 
--- next.config.js
+++ next.config.js
@@ -71,6 +71,10 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'picsum.photos',
         },
+        {
+          protocol: 'https',
+          hostname: 'cdn.loom.com',
+        },
       ],
     },
     async headers() {
