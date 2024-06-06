2908bf80bc45013b8586fc9536105aa1ebc35441
abreel <64946264+abreel@users.noreply.github.com>
2024-06-06 13:30:03 +0100

    added loading spinner, removed template footer reference, updated security & image src in config, and other styling

---
+++ app/projects/loading.tsx
@@ -0,0 +1,6 @@
+import LoadingSpinner from "@/components/LoadingSpinner.tsx";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <LoadingSpinner />
+}
---
+++ components/LoadingSpinner.tsx
@@ -0,0 +1,5 @@
+export default function LoadingSpinner() {
+  return <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
+          <div className="text-lg text-white">Loading...</div>
+        </div>
+}
